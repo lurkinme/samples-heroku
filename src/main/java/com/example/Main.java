b37a32a6e4ab11af280c52a56607b659db2eeb20
Yury Smuraha <yury.smuraha@ihg.com>
2017-09-22 15:28:28 +0300

    Redirect logic

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,88 +1,65 @@
-/*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 @SpringBootApplication
 public class Main {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
-
-  @Autowired
-  private DataSource dataSource;
-
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
   }
 
-  @RequestMapping("/")
-  String index() {
-    return "index";
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+  private static final String DESTINATION = "destination";
+  private RestTemplate restTemplate = new RestTemplate();
+
+  @GetMapping("/")
+  public String hello() {
+    return "Hello World!";
   }
 
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+  @PostMapping(path = "/request", produces = MediaType.TEXT_PLAIN_VALUE)
+  public ResponseEntity<String> notify(@RequestHeader(DESTINATION) String url, @RequestBody String body, @RequestHeader MultiValueMap<String, String> headers) {
+    LOGGER.info("Incoming url {} with body {}", url, body);
+    MultiValueMap<String, String> newHeaders = new LinkedMultiValueMap<>(headers);
+    newHeaders.remove(DESTINATION);
+    HttpEntity<String> entity = new HttpEntity<>(body, newHeaders);
 
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
+    return restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
   }
 
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
+
+  @RequestMapping("/log")
+  public void log(HttpServletRequest request, @RequestBody(required = false) String body, @RequestHeader MultiValueMap<String, String> headers, @RequestParam(required = false) Map<String,String> requestParams) {
+    requestParams = requestParams == null ? Collections.emptyMap() : requestParams;
+    headers = headers == null ? new LinkedMultiValueMap<>() : headers;
+    StringBuilder requestParamsString = new StringBuilder();
+    requestParams.forEach((k,v) -> requestParamsString.append(k).append("=").append(v).append("&")) ;
+    StringBuilder headersString = new StringBuilder();
+    headers.forEach((k,v) -> headersString.append(k).append("=").append(v).append(", "));
+    LOGGER.info("LOG incoming method {} params {} body {} headers {}", request.getMethod(), requestParamsString.toString(), body, headersString);
   }
 
 }
