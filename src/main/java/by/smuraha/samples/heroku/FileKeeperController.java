1e3ba9a8b302d404704feeb9d52e8d02b8993bf0
Yury Smuraha <yury.smuraha@ihg.com>
2017-10-04 17:28:14 +0300

    Add FileKeeperController

---
+++ src/main/java/by/smuraha/samples/heroku/FileKeeperController.java
@@ -0,0 +1,30 @@
+package by.smuraha.samples.heroku;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileKeeperController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileKeeperController.class);
+    private String file = "";
+
+    @PostMapping(path = "/save", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> notify(@RequestBody String body) {
+        LOGGER.info("Incoming file {}", body);
+        file = body;
+        return new ResponseEntity<String>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/.well-known/apple-app-site-association")
+    public ResponseEntity<String> getFile() {
+        return new ResponseEntity<String>(file, HttpStatus.OK);
+    }
+}
